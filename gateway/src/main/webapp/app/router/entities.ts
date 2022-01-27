30a78e001ace8ef5d5de67cd8bef12187f3b2bd0
Matt Raible <matt.raible@okta.com>
2022-01-27 10:02:29 -0700

    Upgrade to JHipster 7.6.0

--- gateway/src/main/webapp/app/router/entities.ts
+++ gateway/src/main/webapp/app/router/entities.ts
@@ -3,17 +3,17 @@ import { Authority } from '@/shared/security/authority';
 // prettier-ignore
 const Entities = () => import('@/entities/entities.vue');
 
-const Post = () => import('@/entities/blog/post/post.vue');
-const PostUpdate = () => import('@/entities/blog/post/post-update.vue');
-const PostDetails = () => import('@/entities/blog/post/post-details.vue');
+const Tag = () => import('@/entities/blog/tag/tag.vue');
+const TagUpdate = () => import('@/entities/blog/tag/tag-update.vue');
+const TagDetails = () => import('@/entities/blog/tag/tag-details.vue');
 
 const Blog = () => import('@/entities/blog/blog/blog.vue');
 const BlogUpdate = () => import('@/entities/blog/blog/blog-update.vue');
 const BlogDetails = () => import('@/entities/blog/blog/blog-details.vue');
 
-const Tag = () => import('@/entities/blog/tag/tag.vue');
-const TagUpdate = () => import('@/entities/blog/tag/tag-update.vue');
-const TagDetails = () => import('@/entities/blog/tag/tag-details.vue');
+const Post = () => import('@/entities/blog/post/post.vue');
+const PostUpdate = () => import('@/entities/blog/post/post-update.vue');
+const PostDetails = () => import('@/entities/blog/post/post-details.vue');
 
 // jhipster-needle-add-entity-to-router-import - JHipster will import entities to the router here
 
@@ -22,27 +22,27 @@ export default {
   component: Entities,
   children: [
     {
-      path: 'post',
-      name: 'Post',
-      component: Post,
+      path: 'tag',
+      name: 'Tag',
+      component: Tag,
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'post/new',
-      name: 'PostCreate',
-      component: PostUpdate,
+      path: 'tag/new',
+      name: 'TagCreate',
+      component: TagUpdate,
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'post/:postId/edit',
-      name: 'PostEdit',
-      component: PostUpdate,
+      path: 'tag/:tagId/edit',
+      name: 'TagEdit',
+      component: TagUpdate,
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'post/:postId/view',
-      name: 'PostView',
-      component: PostDetails,
+      path: 'tag/:tagId/view',
+      name: 'TagView',
+      component: TagDetails,
       meta: { authorities: [Authority.USER] },
     },
     {
@@ -70,27 +70,27 @@ export default {
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'tag',
-      name: 'Tag',
-      component: Tag,
+      path: 'post',
+      name: 'Post',
+      component: Post,
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'tag/new',
-      name: 'TagCreate',
-      component: TagUpdate,
+      path: 'post/new',
+      name: 'PostCreate',
+      component: PostUpdate,
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'tag/:tagId/edit',
-      name: 'TagEdit',
-      component: TagUpdate,
+      path: 'post/:postId/edit',
+      name: 'PostEdit',
+      component: PostUpdate,
       meta: { authorities: [Authority.USER] },
     },
     {
-      path: 'tag/:tagId/view',
-      name: 'TagView',
-      component: TagDetails,
+      path: 'post/:postId/view',
+      name: 'PostView',
+      component: PostDetails,
       meta: { authorities: [Authority.USER] },
     },
     // jhipster-needle-add-entity-to-router - JHipster will add entities to the router here
